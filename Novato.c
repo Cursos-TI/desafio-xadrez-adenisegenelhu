bb6eb4e6e749c6504033f9d59e12705fcd86d6b3
adenisegenelhu <adenisegenelhu@gmail.com>
2025-09-01 15:57:04 +0100

    atualizaçao

---
+++ Novato.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+int main() {
+    // Mover a torre para a direita
+    int torre, i;
+
+    printf("Movimento da Torre\n");
+    printf("Digite quantas casas quer mover a torre: \n");
+    scanf("%d", &torre);
+
+    for (i = 1; i <= torre; i++) {
+        printf("A torre moveu %d casa(s) para a direita!\n", i);
+    }
+
+    // Mover a Rainha em qualquer direção
+    int rainha;
+
+    do {
+        printf("\nMovimento da Rainha \n");
+        printf("1. 5 casas para a direita \n");
+        printf("2. 3 casas para cima \n");
+        printf("3. 8 casas para a esquerda \n");
+        printf("4. Sair \n");
+        printf("Escolha uma opção: ");
+        scanf("%d", &rainha);
+
+        switch (rainha) {
+            case 1:
+                printf("A Rainha moveu 5 casas para a direita! \n");
+                break;
+
+            case 2:
+                printf("A Rainha moveu 3 casas para cima! \n");
+                break;
+
+            case 3:
+                printf("A Rainha moveu 8 casas para a esquerda! \n");
+                break;
+
+            case 4:
+                printf("Saindo do menu da Rainha...\n");
+                break;
+
+            default:
+                printf("Opção inválida! \n");
+        }
+
+    } while (rainha != 4);
+
+    // Mover o Bispo cinco casas na diagonal para cima
+    int bispo = 1;
+
+    printf("\nMovimento do Bispo \n");
+
+    while (bispo <= 5) {
+        printf("O bispo moveu %d casa(s) na diagonal para cima, direita!\n", bispo);
+        bispo++;
+    }
+
+    return 0;
+}
